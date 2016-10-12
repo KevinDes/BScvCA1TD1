b083dcf2500c16256559d348e3984cf67b8a127f
Kevin Descharrieres <kd390560@creusot.u-bourgogne.fr>
2016-10-12 18:42:12 +0200

    update 20161012

---
+++ julia.m
@@ -0,0 +1,10 @@
+function M = julia(zMax, c , N)
+     real = randi([0 zMax], 500);
+     im = randi([0 zMax], 500)*1i;
+     Z = real + im + li;
+     plot(z,'o');
+     M = zeros 
+     
+     
+     
+end
